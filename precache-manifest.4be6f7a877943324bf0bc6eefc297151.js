69b7492a1233e8b9c31d16391a87eb87707da8fb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-26 01:49:55 +0000

    Deploy funnyPan/novel to github.com/funnyPan/novel.git:gh-pages

---
+++ precache-manifest.4be6f7a877943324bf0bc6eefc297151.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef4e2d6efb78e9eb0bbfa17e0b55c52c",
+    "url": "CNAME"
+  },
+  {
+    "revision": "52c936ece5bd15cdd010",
+    "url": "css/Chapter.8e34dec6.css"
+  },
+  {
+    "revision": "22f3f4d710d2944f5e99",
+    "url": "css/app.da8df25e.css"
+  },
+  {
+    "revision": "f5189d30a838d122b092",
+    "url": "css/bookDetail.300849ae.css"
+  },
+  {
+    "revision": "c26eeed093dceac9219b",
+    "url": "css/catSort.3e696c00.css"
+  },
+  {
+    "revision": "0a914cf9b67badcdf10f",
+    "url": "css/category.303a08b4.css"
+  },
+  {
+    "revision": "b843005471afc018d82d",
+    "url": "css/chunk-vendors.62e69cdc.css"
+  },
+  {
+    "revision": "5257341c5f291bd0bbd5deee521eb721",
+    "url": "index.html"
+  },
+  {
+    "revision": "52c936ece5bd15cdd010",
+    "url": "js/Chapter.d408a338.js"
+  },
+  {
+    "revision": "f5700bea61a514bf9709",
+    "url": "js/Search.5180d957.js"
+  },
+  {
+    "revision": "22f3f4d710d2944f5e99",
+    "url": "js/app.350fd56b.js"
+  },
+  {
+    "revision": "f5189d30a838d122b092",
+    "url": "js/bookDetail.287fa4fe.js"
+  },
+  {
+    "revision": "c26eeed093dceac9219b",
+    "url": "js/catSort.8f3477f0.js"
+  },
+  {
+    "revision": "0a914cf9b67badcdf10f",
+    "url": "js/category.afa7e19f.js"
+  },
+  {
+    "revision": "b843005471afc018d82d",
+    "url": "js/chunk-vendors.7f7b2b19.js"
+  },
+  {
+    "revision": "5c67b49217f12a786fbd47a46ad46154",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
